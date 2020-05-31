613e0d7920dedba6cda021bcda81cb98dd011f08
opeco17 <>
2020-06-01 00:24:30 +0900

    fix

--- app/app/src/run.py
+++ app/app/src/run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello World"
-    return name
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(debug=False)
