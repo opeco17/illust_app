8c274955e3054fe891bb095326bf4bba3ea3b8e3
opeco17 <>
2020-06-21 19:38:54 +0900

    add app

---
+++ app/db/mysql_init/initialize.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE image_db;
+
+USE image_db;
+
+CREATE TABLE images(
+  image_id INT AUTO_INCREMENT PRIMARY KEY,
+  image_name VARCHAR(255) not null,
+  hair_color VARCHAR(255),
+  eye_color VARCHAR(255),
+  other_info VARCHAR(255),
+  generated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  used BOOLEAN DEFAULT FALSE
+) ENGINE=INNODB;
+
+
